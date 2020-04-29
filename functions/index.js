eb82a5c11d052fd1fa863395228477f75f44d885
limyifan1 <limyifan1@gmail.com>
2020-04-30 03:45:45 +0800

    Added auto backup of Firestore

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,39 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+const functions = require("firebase-functions");
+const firestore = require("@google-cloud/firestore");
+const client = new firestore.v1.FirestoreAdminClient();
+
+// Replace BUCKET_NAME
+const bucket = "gs://backup-bucket-hawkercentral";
+
+exports.scheduledFirestoreExport = functions.pubsub
+  .schedule("every 24 hours")
+  .onRun((context) => {
+    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+    const databaseName = client.databasePath(projectId, "(default)");
+
+    return client
+      .exportDocuments({
+        name: databaseName,
+        outputUriPrefix: bucket,
+        // Leave collectionIds empty to export all collections
+        // or set to a list of collection IDs to export,
+        // collectionIds: ['users', 'posts']
+        collectionIds: ["hawkers"],
+      })
+      .then((responses) => {
+        const response = responses[0];
+        console.log(`Operation Name: ${response["name"]}`);
+        return response;
+      })
+      .catch((err) => {
+        console.error(err);
+        throw new Error("Export operation failed");
+      });
+  });
+
 // var admin = require("firebase-admin");
 // var functions = require("firebase-functions");
 // const cors = require("cors")({
