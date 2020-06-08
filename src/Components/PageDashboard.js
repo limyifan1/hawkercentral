c7f7a2b7cb05cc7d310a95df7233e863a7c792b0
limyifan1 <limyifan1@gmail.com>
2020-06-08 21:57:19 +0800

    chore(pagedashboard): removed comment

--- src/Components/PageDashboard.js
+++ src/Components/PageDashboard.js
@@ -15,7 +15,6 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { CartContext } from "./themeContext";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-// import placeholder from "../placeholder.png";
 import Button from "@material-ui/core/Button";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import TextField from "@material-ui/core/TextField";
